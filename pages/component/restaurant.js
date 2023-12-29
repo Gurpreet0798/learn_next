c2477d36abd997ff5aa26fa23a80a15c52a3265e
gurpreet <gurpreet@localhost.localdomain>
2023-12-29 15:01:07 +0530

    addedapi

--- pages/component/restaurant.js
+++ pages/component/restaurant.js
@@ -1,18 +1,13 @@
-import React from 'react'
+import React, {useState} from 'react'
+import Menu from './menuApi.js'
+import MenuCard from './menuCard.js'
 
 const Restaurant = () => {
+  const[menuData, setMenuData] = useState(Menu);
   return (
     <>
-     <div className= "card-container">
-      <div className= "card">
-        <div className='card-body'>
-            <span className='card-number card-circle subtle'> 1 </span>
-            <span className='card-author subtle'> Break-Fast </span>
-            <h2 className='card-title'> Maggie </h2>
-            <span className='card-description subtle'></span>
-        </div>
-      </div>
-     </div>
+    
+    <MenuCard menuData={menuData} />
     </>
   )
 }
